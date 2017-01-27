955674ff5a2483b17041444b005a9f1b89257715
annyrtd <fyz95@mail.ru>
2017-01-27 23:59:26 +0300

    smartphone error alert

--- polymino/include.js
+++ polymino/include.js
@@ -1,9 +1,9 @@
 'use strict';
 
-window.onerror = function (message, url, lineNo){
+/*window.onerror = function (message, url, lineNo){
     alert('Error: ' + message + '\n' + 'Line Number: ' + lineNo);
     return true;
-};
+};*/
 
 function supportsES6() {
     "use strict";
