40b0fefe32b25c73efab7079a8768931f30c1040
annyrtd <fyz95@mail.ru>
2017-05-21 18:23:28 +0300

    check if proper number - new logic

--- polymino/src/js/statistics.js
+++ polymino/src/js/statistics.js
@@ -107,14 +107,28 @@ function getNeighbours(node, arr) {
 }
 
 //TODO: add proper check if number of empty cells can be divided by pieces
-function checkIfProperNumber(number) {
-    return true;
+/*function checkIfProperNumber(number) {
     for (let i = 0; i < piecesLength.length; i++) {
         if (number % piecesLength[i] == 0) {
             return true;
         }
     }
     return false;
+}*/
+
+function checkIfProperNumber(number) {
+    if(number < 0) {
+        return false;
+    } else if(number == 0) {
+        return true;
+    } else {
+        let result = false;
+        for (let i = 0; i < piecesLength.length; i++) {
+            result = result || checkIfProperNumber(number - piecesLength[i]);
+        }
+
+        return result;
+    }
 }
 
 export {countStatistic};
